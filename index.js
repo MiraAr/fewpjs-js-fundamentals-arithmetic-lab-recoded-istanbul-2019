74b130cf867d8d4240f1cc0a7b6bf43a0694dd89
mira_arar <mira.ar230@gmail.com>
2020-04-24 13:00:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+//1
+let num1=2;
+let num2= 31;
+let multiply=num1*num2;
+//2
+let random= Math.floor(Math.random()+1);
+//3
+let mod= 24%5;
+//4
+let max= Math.max(1,2,3,20); 
